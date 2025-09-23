7d27171829ff950f1bf8953235d51b592cdefa7b
Csánk Nikolett <niki20061213@gmail.com>
2025-09-23 20:02:40 +0200

    motorcycle - kesz

---
+++ asztali-alkalmazas-fejlesztes-main/Motorcycles/Solution.Core/Models/TypeModel.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Solution.Core.Models;
+
+public partial class TypeModel: ObservableObject
+{
+    [ObservableProperty]
+    private int id;
+
+    [ObservableProperty]
+    private string name;
+
+    public TypeModel()
+    {
+    }
+
+    public TypeModel(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public TypeModel(MotorcycleTypeEntity entity)
+    {
+        if (entity is null)
+        {
+            return;
+        }
+
+        Id = entity.Id;
+        Name = entity.Name;
+    }
+}
